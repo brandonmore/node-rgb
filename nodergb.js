19b6756957909bd996e628f473c8be7ef61952b0
brandon <brandon.more@gmail.com>
2019-05-17 12:16:35 -0400

    updates with hex

--- nodergb.js
+++ nodergb.js
@@ -1,9 +1,9 @@
 const  serialport = require('serialport');
 const fs = require('fs');
 const jpeg = require('jpeg-js');
-const packet_size = 7;
-var myPort = new serialport('COM4', {
-  baudRate: 9600
+const packet_size = 18;
+var myPort = new serialport('/dev/ttyACM0', {
+  baudRate: 38400
 })
 // serialport.list(function (err, ports) {
 //   ports.forEach(function(port) {
@@ -56,28 +56,29 @@ for(let i=0;i<newdata.length;i++){
     }
     count++;
 }
+
 //console.log(finaldata[0]);
 //console.log(finaldata[0]);
 var createPixel = (rgb) => {
   return '<span style="background-color:rgba('+rgb+')"></span>';
 }
 //console.log(jpegImageData);
-// var markup = "<html><body><style>div{overflow:hidden;}span{border:2px solid black;width:5px;height:5px;float:left;}</style><div>";
-// newdata.map((rgb,index)=>{
-//   if(index%rawImageData.width == 0)
-//     markup+='</div><div>';
-//   markup+=createPixel(rgb);
-// })
-// markup+='</div>';
-// markup+= '</body></html>';
-// fs.writeFile('rgb_new.html', markup, (err) => {
-//         if (err) throw err;
-//         console.log('The file has been saved yeah!');
-//       });
-// fs.writeFile('rgb_new.txt', newdata.join(','), (err) => {
-//   if (err) throw err;
-//     console.log('The file has been saved yeah!');
-//  });
+var markup = "<html><body><style>div{overflow:hidden;}span{border:2px solid black;width:5px;height:5px;float:left;}</style><div>";
+newdata.map((rgb,index)=>{
+  if(index%rawImageData.width == 0)
+    markup+='</div><div>';
+  markup+=createPixel(rgb);
+})
+markup+='</div>';
+markup+= '</body></html>';
+fs.writeFile('rgb_new.html', markup, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved yeah!');
+      });
+fs.writeFile('rgb_new.txt', newdata.join(','), (err) => {
+  if (err) throw err;
+    console.log('The file has been saved yeah!');
+ });
 
 function componentToHex(c) {
     var hex = c.toString(16);
@@ -94,11 +95,28 @@ var iter = 0;
 var inter = {};
 setTimeout(function(){
     //myPort.write(finaldata[0]+"\n");
+    let load_date = new Date().getTime();
     inter = setInterval(function(){
-        if(iter>=finaldata.length-1)
-            clearInterval(inter);
-        console.log(finaldata[iter]);
+        if(iter>=finaldata.length){
+        	clearInterval(inter);
+        	let finish_date = new Date().getTime();
+        	console.log(finish_date-load_date);
+        }
+               
+        // console.log(newdata[iter]);
+        // console.log(finaldata[iter]);
         myPort.write(finaldata[iter]+"\n");
+        //myPort.write("008080008080008080008080008080008080008080008080008080008080008080008080008080008080008080008080008080008080"+"\n");
+        //console.log(iter);
         iter++;
-    },50);
+    },32);
+
+    // for(let i=0;i<finaldata.length;i++){
+    // 	console.log(newdata[i]);
+    //     //console.log(finaldata[i]);
+    // }
+    // for(let i=0;i<finaldata.length;i++){
+    // 	//console.log(newdata[i]);
+    //     console.log(finaldata[i]);
+    // }
 },1000);
